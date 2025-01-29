6b68470b8c982e870d2028aa895e46ed5d656624
maverickyurii <yura.spivak94@gmail.com>
2025-01-29 21:57:39 +0200

    warap buttons

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,16 +1,13 @@
-import { Context } from 'telegraf';
-import { Markup } from 'telegraf';
+import { Context, Markup } from 'telegraf';
 
 export const startCommand = async (ctx: Context) => {
     await ctx.reply(
         'Greetings! I am a bot for creating Solana wallets. Use the buttons below to take action:',
         Markup.inlineKeyboard([
-            Markup.button.callback('Create Wallet', 'create_wallet'),
-            Markup.button.callback('Check Balance', 'check_balance'),
-            Markup.button.callback('Load Wallet', 'load_wallet'),
-            Markup.button.callback('Send Solana', 'send_sol'),
-            Markup.button.callback('Request Airdrop', 'load_airdrop'),
-            Markup.button.callback('Show Wallets', 'show_wallets')
+            [Markup.button.callback('Create Wallet', 'create_wallet'), Markup.button.callback('Load Wallet', 'load_wallet')],
+            [Markup.button.callback('Check Balance', 'check_balance'), Markup.button.callback('Show Wallets', 'show_wallets')],
+            [Markup.button.callback('Send Solana', 'send_sol'), Markup.button.callback('Request Airdrop', 'load_airdrop')]
         ])
     );
 };
+
